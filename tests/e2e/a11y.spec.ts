9185271c9eb1f56edf3936df2eaf281fabc7b38b
hffeka <165193174+hffeka@users.noreply.github.com>
2024-09-18 16:28:31 +0300

    test: add example tests

---
+++ tests/e2e/a11y.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from "playwright/test";
+import AxeBuilder from "@axe-core/playwright";
+
+test("has no immediate accessibility issues", async ({ page }) => {
+	await page.goto("/");
+	const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+	expect(accessibilityScanResults.violations).toEqual([]);
+});
---
+++ tests/e2e/index.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "playwright/test";
+
+test("index page exists and opens", async ({ page }) => {
+	const response = await page.goto("/");
+	expect(response).toBeTruthy();
+	expect(response?.status()).toBeLessThan(400);
+});
+
+test("index page has title", async ({ page }) => {
+	await page.goto("/");
+	await expect(page).toHaveTitle(/Gatika/);
+});
