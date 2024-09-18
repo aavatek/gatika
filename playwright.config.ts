7e7e36f94a0ed289910e4d0ecac08bd5e7b2cd3a
hffeka <165193174+hffeka@users.noreply.github.com>
2024-09-18 13:23:53 +0300

    feat: configure playwright

---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+	testDir: "e2e",
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: [["html", { open: "never" }]],
+
+	use: {
+		headless: true,
+		baseURL: "http://127.0.0.1:3000",
+		trace: "on-first-retry",
+	},
+
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+	],
+
+	webServer: {
+		command: "bun dev",
+		url: "http://127.0.0.1:3000",
+		timeout: 5000,
+		reuseExistingServer: !process.env.CI,
+	},
+});
