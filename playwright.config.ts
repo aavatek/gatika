9e4e6e7b38c8ea2b16a138752e06316fa8e99c90
hffeka <165193174+hffeka@users.noreply.github.com>
2024-09-18 14:31:45 +0300

    fix(playwright): allow os to resolve loopback address

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 
 	use: {
 		headless: true,
-		baseURL: "http://127.0.0.1:3000",
+		baseURL: "http://localhost:3000",
 		trace: "on-first-retry",
 	},
 
@@ -23,7 +23,7 @@ export default defineConfig({
 
 	webServer: {
 		command: "bun dev",
-		url: "http://127.0.0.1:3000",
+		url: "http://localhost:3000",
 		timeout: 5000,
 		reuseExistingServer: !process.env.CI,
 	},
