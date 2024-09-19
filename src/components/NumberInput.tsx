963e349960a6ff876b8f0aaa62fbb4bee852fec6
hffeka <165193174+hffeka@users.noreply.github.com>
2024-09-19 20:05:42 +0300

    feat: implement initial NumberInput component

---
+++ src/components/NumberInput.tsx
@@ -0,0 +1,30 @@
+import { createMemo, splitProps, type ComponentProps } from "solid-js";
+
+type NumberInputProps = {
+	name: string;
+	label: string;
+	value: number | undefined;
+	required?: boolean;
+	class?: string;
+	min?: number;
+	max?: number;
+	step?: number;
+} & ComponentProps<"input">;
+
+export function NumberInput(props: NumberInputProps) {
+	const [, inputProps] = splitProps(props, ["class", "label", "value"]);
+
+	const getValue = createMemo(() => {
+		const value = props.value;
+		return value !== undefined && !Number.isNaN(value) ? value : "";
+	});
+
+	return (
+		<div class={props.class}>
+			<label for={props.name}>
+				{props.label} {props.required && <span>*</span>}
+			</label>
+			<input {...inputProps} id={props.name} type="number" value={getValue()} />
+		</div>
+	);
+}
