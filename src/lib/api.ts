e7e65cc6080523b71118bb77da29da4efd16fce8
hffeka <165193174+hffeka@users.noreply.github.com>
2024-09-28 14:28:09 +0300

    feat: implement api layer

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,25 +1,39 @@
-const api = {
-	tasks: {
-		create: () => {},
-		read: () => {},
-		update: () => {},
-		delete: () => {},
-		list: () => {},
+import { makePersisted, storageSync } from '@solid-primitives/storage';
+import { createMemo } from 'solid-js';
+import { createStore, produce } from 'solid-js/store';
+import type { Task } from '../components/Task';
+
+export const [taskStore, setTaskStore, init] = makePersisted(
+	createStore<Task[]>([]),
+	{
+		name: 'taskStore',
+		storage: localStorage,
+		sync: storageSync,
+	},
+);
+
+export const tasks = {
+	create: (task: Task) => {
+		setTaskStore(
+			produce((store) => {
+				store.push(task);
+			}),
+		);
 	},
 
-	projects: {
-		create: () => {},
-		read: () => {},
-		update: () => {},
-		delete: () => {},
-		list: () => {},
+	read: (id: string) =>
+		createMemo(() => taskStore.find((task) => task.id === id)),
+
+	update: (id: string, updatedTask: Partial<Task>) => {
+		setTaskStore(
+			(task) => task.id === id,
+			produce((task) => Object.assign(task, updatedTask)),
+		);
 	},
 
-	worksites: {
-		create: () => {},
-		read: () => {},
-		update: () => {},
-		delete: () => {},
-		list: () => {},
+	delete: (id: string) => {
+		setTaskStore((store) => store.filter((task) => task.id !== id));
 	},
+
+	list: () => taskStore,
 };
