9b0bdd6768820eb3f70e64188eac4e44b0f5d85a
hffeka <165193174+hffeka@users.noreply.github.com>
2024-09-28 14:43:40 +0300

    feat: create TaskList component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,54 +1,52 @@
-import type { SubmitHandler } from '@modular-forms/solid';
 import { createForm, reset, valiForm } from '@modular-forms/solid';
-import type { InferOutput } from 'valibot';
+import type { SubmitHandler } from '@modular-forms/solid';
+import { useNavigate } from '@solidjs/router';
+import { For } from 'solid-js';
 import * as v from 'valibot';
+import { tasks } from '../lib/api';
 import { Button } from './form/Button';
-import { DateField } from './form/DateField';
-import { TextField } from './form/TextField';
+import { InputField } from './form/InputField';
 
-const TaskInputSchema = v.object({
-	name: v.pipe(v.string('Invalid'), v.nonEmpty('Required')),
-	startDate: v.date(),
-	endDate: v.date(),
-});
+const TaskSchema = v.pipe(
+	v.object({
+		name: v.pipe(v.string(), v.nonEmpty('Required')),
+		startDate: v.optional(v.date()),
+		endDate: v.optional(v.date()),
+	}),
 
-const TaskSchema = v.object({
-	...TaskInputSchema.entries,
-	id: v.pipe(v.string(), v.uuid()),
-	created: v.date(),
-});
+	v.transform((input) => ({
+		...input,
+		id: crypto.randomUUID(),
+		created: new Date().toISOString(),
+	})),
+);
 
-type TaskInput = InferOutput<typeof TaskInputSchema>;
-type Task = InferOutput<typeof TaskSchema>;
+export type TaskInput = v.InferInput<typeof TaskSchema>;
+export type Task = v.InferOutput<typeof TaskSchema>;
 
 export const TaskForm = () => {
 	const [createTaskForm, { Form, Field }] = createForm<TaskInput>({
-		validate: valiForm(TaskInputSchema),
+		validate: valiForm(TaskSchema),
 	});
 
 	const handleSubmit: SubmitHandler<TaskInput> = (data, _) => {
-		const task: Task = {
-			id: crypto.randomUUID(),
-			created: new Date(),
-			...data,
-		};
-
-		// TODO: do something with task here
-		console.log(task);
-
-		// reset form after
-		reset(createTaskForm);
+		try {
+			tasks.create(v.parse(TaskSchema, data));
+			reset(createTaskForm);
+		} catch (e) {
+			console.error(e);
+		}
 	};
 
 	return (
-		<Form onSubmit={handleSubmit}>
+		<Form onSubmit={handleSubmit} class="task-form">
 			<Field name="name">
 				{(field, props) => (
-					<TextField
+					<InputField
 						{...props}
 						type="text"
-						label="Name"
-						placeholder="Task Name"
+						label="Tehtävän nimi"
+						placeholder="esim. Liittymätyö"
 						value={field.value}
 						error={field.error}
 					/>
@@ -57,17 +55,57 @@ export const TaskForm = () => {
 
 			<Field name="startDate" type="Date">
 				{(field, props) => (
-					<DateField {...props} value={field.value} label="Start Date" />
+					<InputField
+						{...props}
+						type="date"
+						label="Alkaa"
+						value={field.value}
+						error={field.error}
+					/>
 				)}
 			</Field>
 
 			<Field name="endDate" type="Date">
 				{(field, props) => (
-					<DateField {...props} value={field.value} label="End Date" />
+					<InputField
+						{...props}
+						type="date"
+						label="Päättyy (suunniteltu)"
+						value={field.value}
+						error={field.error}
+					/>
 				)}
 			</Field>
 
-			<Button type="submit" content="Create" />
+			<Button type="submit" content="Luo tehtävä" />
 		</Form>
+	);
+};
+
+export const TaskList = () => {
+	return (
+		<section>
+			<h2>Tehtävät</h2>
+			<ul class="task-list">
+				<For each={tasks.list()}>
+					{(task: Task) => <TaskListItem {...task} />}
+				</For>
+			</ul>
+		</section>
+	);
+};
+
+const TaskListItem = (props: Task) => {
+	const navigate = useNavigate();
+
+	const handleViewTask = () => {
+		navigate(`/tasks/${props.id}`);
+	};
+
+	return (
+		<li class="task-list-item">
+			<span>{props.name}</span>
+			<Button content="Hallitse" onclick={handleViewTask} />
+		</li>
 	);
 };
