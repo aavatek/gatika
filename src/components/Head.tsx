4f057a8082b1b9eea14ab5f1881891edb12dc96a
hffeka <165193174+hffeka@users.noreply.github.com>
2024-09-28 14:38:39 +0300

    refactor(Head): move parts of metadata into index.html

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -1,14 +1,11 @@
-import favicon from '$assets/favicon.ico';
-import { Link, Meta, Title } from '@solidjs/meta';
+import { Meta, Title } from '@solidjs/meta';
 
 export default function Head() {
 	return (
 		<>
 			<Title>Gatika</Title>
-			<Meta charset="utf-8" />
 			<Meta name="author" content="AavaTek" />
-			<Meta name="viewport" content="width=device-width, initial-scale=1" />
-			<Link rel="icon" href={favicon} />
+			<Meta name="description" content="PoC Gantt project management app" />
 		</>
 	);
 }
