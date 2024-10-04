f9921dc3add1aab0aa844872aaad070f55ceb1f7
marbellu <marika.bergman@tuta.com>
2024-10-04 09:29:51 +0300

    test: implement testing for field error component

--- src/components/form/FieldError.test.tsx
+++ src/components/form/FieldError.test.tsx
@@ -1,7 +1,29 @@
-import { describe, it } from 'vitest';
+import { FieldError } from './FieldError';
+import { render } from '@solidjs/testing-library';
+import { createSignal } from 'solid-js';
+import { describe, expect, it } from 'vitest';
+
+const errorMessage = 'Error is present';
 
 describe('FieldError', () => {
-	it('should render an error message', () => {
-		// TODO: write tests
+	it('renders an error message', () => {
+		const { getByText } = render(() => <FieldError error={errorMessage} />);
+
+		const fieldError = getByText(errorMessage);
+		expect(fieldError.textContent).toBe(errorMessage);
+	});
+
+	it('has appropriate aria attribute', () => {
+		const [props, setProps] = createSignal({});
+
+		const { container } = render(() => (
+			<FieldError {...props} aria-disabled="true" />
+		));
+
+		const fieldError = container.querySelector('output') as HTMLOutputElement;
+		expect(fieldError).toHaveAttribute('aria-hidden', 'true');
+
+		setProps({ error: errorMessage });
+		expect(fieldError).not.toHaveAttribute('aria-hidden');
 	});
 });
