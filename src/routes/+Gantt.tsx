81ffc331ce3e56ddb269376949b3361e6da2efcd
hffeka <165193174+hffeka@users.noreply.github.com>
2024-10-10 18:49:23 +0300

    feat: list tasks from selected project

--- src/routes/+Gantt.tsx
+++ src/routes/+Gantt.tsx
@@ -1,16 +1,31 @@
-import { createEffect, createSignal } from 'solid-js';
-import { SelectField } from '../components/Form';
-import { projects } from '../features/Project';
+import { createEffect, createMemo, createSignal, For } from 'solid-js';
+import { SelectField } from '@components/Form';
+import { projects, type Project } from '@features/Project';
+import { tasks } from '@features/Task';
 
 export const Gantt = () => {
 	const [selected, setSelected] = createSignal('');
 	const projectSelectOptions = projects.list().map((p) => ({
 		label: p.name,
-		value: p.id,
+		value: p.id as Project['id'],
 	}));
 
 	createEffect(() => {
 		console.log(selected());
+	});
+
+	const ganttTasks = createMemo(() => {
+		return [
+			...(selected() === ''
+				? tasks.list()
+				: tasks.listByProject(selected() as Project['id'])),
+		].map((item) => ({
+			id: item.id,
+			name: item.name,
+			start: item.startDate,
+			end: item.endDate,
+			dependencies: item.dependencies?.join(', ') || undefined,
+		}));
 	});
 
 	return (
@@ -22,6 +37,8 @@ export const Gantt = () => {
 				value={selected()}
 				onChange={(e) => setSelected(e.target.value)}
 			/>
+
+			<For each={ganttTasks()}>{(task) => <p>{task.name}</p>}</For>
 		</main>
 	);
 };
