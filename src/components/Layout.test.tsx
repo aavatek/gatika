9c688581d9a9d41f1ce6950f27d923b8af91bd96
Laura Launonen <laura.launonen@tuni.fi>
2024-10-13 21:17:07 +0300

    test: add layout tests

--- src/components/Layout.test.tsx
+++ src/components/Layout.test.tsx
@@ -1,13 +1,48 @@
-import { describe } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
+import { render } from '@solidjs/testing-library';
+import { Layout } from './Layout';
+import { MetaProvider } from '@solidjs/meta';
 
-describe.todo('Layout', () => {
-	// TODO: write tests for Layout
-});
+vi.mock('./Nav', () => ({
+	Nav: () => (
+		<nav>
+			<ul>
+				<li>Mock nav component</li>
+			</ul>
+		</nav>
+	),
+}));
 
-describe.todo('Header', () => {
-	// TODO: write tests for Header
-});
+describe('Layout component', () => {
+	it('renders children passed to it', () => {
+		const { getByText } = render(() => (
+			<MetaProvider>
+				<Layout>
+					<div>Layout test</div>
+				</Layout>
+			</MetaProvider>
+		));
+		expect(getByText('Layout test')).toBeInTheDocument();
+	});
 
-describe.todo('Meta', () => {
-	// TODO: write tests for Meta
+	it('renders Header', () => {
+		const { container } = render(() => (
+			<MetaProvider>
+				<Layout />
+			</MetaProvider>
+		));
+		expect(container.querySelector('header')).toBeInTheDocument();
+	});
+
+	it('renders meta tags', () => {
+		render(() => (
+			<MetaProvider>
+				<Layout />
+			</MetaProvider>
+		));
+		expect(document.querySelector('meta=[name="author"]')).toBeInTheDocument();
+		expect(
+			document.querySelector('meta[name="description"]'),
+		).toBeInTheDocument();
+	});
 });
