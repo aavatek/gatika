a76ce282ea16574297c40ba0e0794effe35b9205
hffeka <165193174+hffeka@users.noreply.github.com>
2024-10-14 16:05:36 +0300

    feat: naive gantt implementation

--- src/routes/+Gantt.tsx
+++ src/routes/+Gantt.tsx
@@ -5,11 +5,11 @@ import { tasks } from '@features/Task';
 
 export const Gantt = () => {
 	const [selected, setSelected] = createSignal('');
+	const [distance, setDistance] = createSignal(30);
 	const projectSelectOptions = projects.list().map((p) => ({
 		label: p.name,
 		value: p.id as Project['id'],
 	}));
-
 	const ganttTasks = createMemo(() => {
 		return [
 			...(selected() === ''
@@ -17,6 +17,24 @@ export const Gantt = () => {
 				: tasks.listByProject(selected() as Project['id'])),
 		];
 	});
+	const ganttTasksWithDate = createMemo(() => {
+		return ganttTasks()
+			.filter((task) => task.start && task.duration)
+			.map((task) => ({ ...task, start: new Date(task.start as Date) }));
+	});
+	const earliestStartDate = createMemo(() => {
+		const dates = ganttTasksWithDate().map((task) => task.start);
+		return new Date(Math.min(...dates.map((d) => d.getTime())));
+	});
+	const [chartStartDate, setChartStartDate] = createSignal(earliestStartDate());
+
+	const handleResize = (e: MouseEvent, gridSize: number) => {
+		const target = e.target as HTMLElement;
+		const rect = target.getBoundingClientRect();
+		const x = e.clientX - rect.left;
+		const snappedWidth = Math.round(x / gridSize) * gridSize;
+		target.style.width = `${snappedWidth}px`;
+	};
 
 	return (
 		<main>
@@ -28,8 +46,58 @@ export const Gantt = () => {
 				value={selected()}
 				onChange={(e) => setSelected(e.target.value)}
 			/>
+			<div class="gantt">
+				<For each={ganttTasksWithDate()}>
+					{(task) => {
+						const length = createMemo(() => {
+							return task.duration ? (task.duration / distance()) * 100 : 15;
+						});
+						const startOffset = createMemo(() => {
+							const diffTime =
+								task.start.getTime() - chartStartDate().getTime();
+							const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+							return diffDays + 1;
+						});
 
-			<For each={ganttTasks()}>{(task) => <p>{task.name}</p>}</For>
+						return (
+							<div
+								class="gantt-row"
+								style={{
+									width: '100%',
+									border: '1px solid black',
+									display: 'grid',
+									'grid-template-columns': `repeat(${distance()}, 1fr)`,
+								}}
+							>
+								<div
+									class="gantt-row-item"
+									style={{
+										border: '1px solid black',
+										'grid-column': `${startOffset()} / span ${Math.ceil(length() / (100 / distance()))}`,
+										'min-width': '0',
+										overflow: 'hidden',
+										resize: 'horizontal',
+										position: 'relative',
+									}}
+									onMouseUp={(e) => handleResize(e, 100 / distance())}
+								>
+									{task.name}
+									<div
+										style={{
+											position: 'absolute',
+											right: '0',
+											top: '0',
+											bottom: '0',
+											width: '5px',
+											cursor: 'col-resize',
+										}}
+									/>
+								</div>
+							</div>
+						);
+					}}
+				</For>
+			</div>
 		</main>
 	);
 };
