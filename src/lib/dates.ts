aa5b5e8eb4fb563032d56e3379c7ef575b44e849
hffeka <165193174+hffeka@users.noreply.github.com>
2024-10-16 15:12:40 +0300

    feat: create datetime helpers

---
+++ src/lib/dates.ts
@@ -0,0 +1,9 @@
+export const formatTime = (epoch: number) => {
+	const date = new Date(epoch);
+
+	const day = String(date.getDate()).padStart(2, '0');
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const year = date.getFullYear();
+
+	return `${day}.${month}.${year}`;
+};
