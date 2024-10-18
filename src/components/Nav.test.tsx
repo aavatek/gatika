e89b17dc982a29e28b9d305fe945ee3e9e84f185
Laura Launonen <laura.launonen@tuni.fi>
2024-10-18 16:59:02 +0300

    test: add nav tests

--- src/components/Nav.test.tsx
+++ src/components/Nav.test.tsx
@@ -1,9 +1,34 @@
-import { describe } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
+import { render } from '@solidjs/testing-library';
+import { Nav } from './Nav';
+import type { AnchorProps } from '@solidjs/router';
 
 // -------------------------------------------------------------------------------------
 
-describe.todo('Nav', () => {
-	// TODO: write tests for Nav
+vi.mock('@solidjs/router', () => ({
+	A: (props: AnchorProps) => <a href={props.href}>{props.children}</a>,
+}));
+
+describe('Nav Component', () => {
+	it('renders nothing when there are no items', () => {
+		const { queryByRole } = render(() => <Nav items={[]} />);
+		expect(queryByRole('navigation')).toBeNull();
+	});
+
+	it('renders a list of links when there are multiple items', () => {
+		const items: AnchorProps[] = [
+			// create 2 links: "Home" & "About"
+			{ href: '/home', children: 'Home' },
+			{ href: '/about', children: 'About' },
+		];
+
+		const { getByRole, getAllByRole, getByText } = render(() => (
+			<Nav items={items} />
+		));
+
+		expect(getByText('Home')).toBeInTheDocument();
+		expect(getByText('About')).toBeInTheDocument();
+		expect(getByRole('list')).toBeInTheDocument();
+		expect(getAllByRole('listitem')).toHaveLength(2);
+	});
 });
-
-// -------------------------------------------------------------------------------------
