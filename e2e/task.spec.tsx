f77f4c83c06a198d678c2da17f732780cacf04f9
Laura Launonen <laura.launonen@tuni.fi>
2024-11-15 19:39:49 +0200

    test: modify task tests

--- e2e/task.spec.tsx
+++ e2e/task.spec.tsx
@@ -1,11 +1,13 @@
 import { expect, test } from 'playwright/test';
 
-test('Check the tasks view', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
 	await page.goto('/projects');
 	await page.fill('label:has-text("Projektin nimi")', 'Projekti X');
 	await page.click('button:has-text("Luo")');
 	await page.getByText('Projekti X').click();
+});
 
+test('Check the tasks view', async ({ page }) => {
 	// Check content
 	await expect(page.locator('h1')).toHaveText('Projekti X');
 	expect(page.getByText('Kaikki tehtävät'));
@@ -14,18 +16,11 @@ test('Check the tasks view', async ({ page }) => {
 });
 
 test('create, edit and remove a task', async ({ page }) => {
-	// create a project and a task for it
-	await page.goto('/projects');
-	await page.fill('label:has-text("Projektin nimi")', 'Projekti X');
-	await page.click('button:has-text("Luo")');
-	await page.getByText('Projekti X').click();
-
 	await page.getByRole('textbox', { name: 'Tehtävän nimi' }).fill('Tehtävä X1');
 	await page.selectOption('[name="type"]', { label: 'FaultRepair' });
 	await page.selectOption('[name="status"]', { label: 'Suunniteltu' });
 	await page.click('button:has-text("Luo tehtävä")');
 
-	// edit the task
 	await page.getByText('Tehtävä X1').click();
 	await page.getByRole('textbox', { name: 'Tehtävän nimi' }).fill('Tehtävä Y1');
 	await page.selectOption('[name="type"]', { label: 'InvestmentTask' });
@@ -33,20 +28,28 @@ test('create, edit and remove a task', async ({ page }) => {
 	await page.getByRole('button', { name: 'Takaisin' }).click();
 	await expect(page.locator('a', { hasText: 'Tehtävä Y1' })).toBeVisible();
 
-	// remove a task
 	await page.getByText('Tehtävä Y1').click();
 	await page.getByRole('button', { name: 'Poista' }).click();
 	await expect(page.getByText('Tehtävä Y1')).not.toBeVisible();
 });
 
 test('show error when creating a task without name', async ({ page }) => {
-	await page.goto('/projects');
-	await page.fill('label:has-text("Projektin nimi")', 'Projekti X');
-	await page.click('button:has-text("Luo")');
-	await page.getByText('Projekti X').click();
-
 	await page.click('button:has-text("Luo tehtävä")');
 	await expect(page.getByText('Anna tehtävälle nimi')).toBeVisible();
 });
 
-// add more tests
+test('create task with only  date', async ({ page }) => {
+	await page.getByRole('textbox', { name: 'Tehtävän nimi' }).fill('Tehtävä 1');
+	await page.fill('[name="start"]', '2024-12-01');
+	await page.click('button:has-text("Luo tehtävä")');
+	await expect(page.locator('a', { hasText: 'Tehtävä 1' })).toBeVisible();
+	await expect(page.getByText('01.12.2024')).toBeVisible();
+});
+
+test('create task with only start date', async ({ page }) => {
+	await page.getByRole('textbox', { name: 'Tehtävän nimi' }).fill('Tehtävä 1');
+	await page.fill('[name="end"]', '2025-01-01');
+	await page.click('button:has-text("Luo tehtävä")');
+	await expect(page.locator('a', { hasText: 'Tehtävä 1' })).toBeVisible();
+	await expect(page.getByText('01.01.2025')).toBeVisible();
+});
