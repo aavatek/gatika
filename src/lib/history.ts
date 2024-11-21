546708de468318d584648844e5700c658e8cd5d3
marbellu <marika.bergman@tuta.com>
2024-11-21 13:19:28 +0200

    feat(Gantt): implement redo

--- src/lib/history.ts
+++ src/lib/history.ts
@@ -1,4 +1,4 @@
-import { setTaskStore, type Task } from '@features/Task';
+import { setTaskStore, taskStore, type Task } from '@features/Task';
 import { batch } from 'solid-js';
 import { unwrap } from 'solid-js/store';
 
@@ -8,7 +8,6 @@ const MAX_HISTORY = 4;
 
 export const handleHistory = (state: Task[]) => {
 	const copy = structuredClone(unwrap(state));
-
 	if (
 		undoStack.length > 0 &&
 		JSON.stringify(copy) === JSON.stringify(undoStack[undoStack.length - 1])
@@ -25,16 +24,24 @@ export const handleHistory = (state: Task[]) => {
 
 export const undo = () => {
 	if (undoStack.length === 0) return;
-
-	undoStack.pop();
 	const latestState = undoStack.pop();
 
-	if (latestState !== undefined) {
+	if (latestState) {
 		batch(() => {
-			redoStack.push(latestState);
+			redoStack.push(structuredClone(unwrap(taskStore)));
 			setTaskStore(latestState);
 		});
 	}
 };
 
-export const redo = () => {};
+export const redo = () => {
+	if (redoStack.length === 0) return;
+	const latestState = redoStack.pop();
+
+	if (latestState) {
+		batch(() => {
+			undoStack.push(structuredClone(unwrap(taskStore)));
+			setTaskStore(latestState);
+		});
+	}
+};
